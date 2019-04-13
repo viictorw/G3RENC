c113150c83c5b0b60fcb7463bd2bbbe08e78f172
viictorw <viictor.wagner@gmail.com>
2019-04-13 15:59:09 -0300

    att Controller

--- src/java/controller/ManterAdministradorController.java
+++ src/java/controller/ManterAdministradorController.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package controller.Administrador;
+package controller;
 
 import java.io.IOException;
 import java.sql.SQLException;
--- src/java/controller/PesquisaAdminController.java
+++ src/java/controller/PesquisaAdminController.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package controller.Administrador;
+package controller;
 import model.Administrador;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
