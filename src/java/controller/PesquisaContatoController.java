38df8e380009a071a4105ea67deb4426f1b42501
izaRibeiro <bellarpjf@gmail.com>
2019-04-13 23:03:48 -0300

    Ops

---
+++ src/java/controller/PesquisaContatoController.java
@@ -0,0 +1,37 @@
+package controller;
+
+import model.Contato;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import java.sql.SQLException;
+
+public class    PesquisaContatoController extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    protected void processRequest(HttpServletRequest request,
+                                  HttpServletResponse response) throws SecurityException, IOException, ServletException {
+        try {
+            request.setAttribute("contatos", Contato.obterTodosContatoes());
+            RequestDispatcher view = request.getRequestDispatcher("/pesquisaContato.jsp");
+            view.forward(request, response);
+        } catch (ClassNotFoundException | SQLException e) {
+            throw new ServletException(e);
+        } catch (ServletException e) {
+        }
+    }
+}
